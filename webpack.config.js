76dc16dc47e4d7d342c69e95b0bf1760e2e92e20
Luis Fuentes <lfuentesreyes@gmail.com>
2015-07-19 17:00:41 -0300

    add dev server

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,40 @@
 var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var merge = require('webpack-merge');
 
-module.exports = {
-  entry: path.resolve(__dirname, 'app/main'),
+var TARGET = process.env.TARGET;
+var ROOT_PATH = path.resolve(__dirname);
+
+var common = {
+  entry: [path.resolve(ROOT_PATH, 'app/main')],
   output: {
-    path: path.resolve(__dirname, 'build'),
+    path: path.resolve(ROOT_PATH, 'build'),
     filename: 'bundle.js',
   },
+  module: {
+    loaders: [
+      {
+        test: /\.css$/,
+        loaders: ['style', 'css'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Webpack app',
+    }),
+  ],
 };
+
+if(TARGET === 'build') {
+  module.exports = common;
+}
+
+if(TARGET === 'dev') {
+  module.exports = merge(common, {
+    entry: [
+      'webpack-dev-server/client?http://localhost:8080',
+      'webpack/hot/dev-server'
+    ]
+  });
+}
